3bc9bd31952387851c982700e49e7b41c0248eb6
arbezerra <arbezerra@gmail.com>
2023-12-04 03:12:41 -0300

    feat: Day 01 - Part1

---
+++ day-01/src/part1.rs
@@ -0,0 +1,40 @@
+fn main() {
+    let input = include_str!("./part1.txt");
+    let output = process(input);
+    println!("{}", output);
+}
+
+fn process(input: &str) -> String {
+    let mut total = 0;
+    for line in input.lines() {
+        let mut first = -1;
+        let mut last = -1;
+        for c in line.chars() {
+            if c.is_numeric() {
+                if first == -1 {
+                    first = c as i32 - 0x30;
+                } else {
+                    last = c as i32 - 0x30;
+                }
+            }
+        }
+        if last == -1 {
+            last = first
+        }
+
+        total += 10 * first + last;
+    }
+    return total.to_string();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let input = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet";
+        let out = process(input);
+        assert_eq!(out, "142");
+    }
+}
